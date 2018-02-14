368c5d492ac26b1994860c2d13cc9562f30cb6a0
Seifeddine Largat <largatseif@outlook.com>
2018-02-14 11:58:51 +0100

    just a menu to choose which magazine to scrap

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Scrapping tool</title>
+</head>
+<body>
+    Select magazine to scrap :
+    <ul>
+        <li><a href="eanlibya.php" >EANLIBYA</a></li>
+        <li><a href="albasma.php" >ALBASMA</a></li>
+        <li><a href="alwasat.php" >ALWASAT</a></li>
+        <li><a href="libyaakhbart.php" >LIBYAAKHBART</a></li>
+    </ul>
+</body>
+</html>
